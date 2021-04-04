072d47644263ee98839889400ea4db6cd8ba777e
Michael Mason <mikemason930@gmail.com>
2021-04-04 14:50:37 -0400

    Target C++ 2x in install script

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -std=c++0x -DXP_UNIX -I/usr/local/include -I/usr/local/lib/pkgconfig/../../include -I/usr/local/include -o ./sqlDatabase.o -c ./sqlDatabase.cpp
+g++ -std=c++2a -DXP_UNIX -I/usr/local/include -I/usr/local/lib/pkgconfig/../../include -I/usr/local/include -o ./sqlDatabase.o -c ./sqlDatabase.cpp
 ar rc ./libsqlDatabase.a ./sqlDatabase.o
 mv ./libsqlDatabase.a /usr/local/lib/libsqlDatabase.a
 cp ./sqlDatabase.h /usr/include/mysql/sqlDatabase.h
