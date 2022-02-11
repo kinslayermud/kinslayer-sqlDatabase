a53f07c952cd393cbc6f22a4aab434c53ff26ec8
Michael Mason <mikemason930@gmail.com>
2022-02-11 14:18:18 -0500

    Additional warning flags

--- mysql/install.sh
+++ mysql/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-g++ -std=c++2a -Wformat-security -Wformat -Wpointer-arith -Wcast-align -Wredundant-decls -Wcatch-value -Werror -I/usr/local/include -o ./sqlDatabase.o -c ./sqlDatabase.cpp
+g++ -std=c++2a -Wformat-security -Wformat -Wpointer-arith -Wcast-align -Wredundant-decls -Wcatch-value -Wpedantic -Wall -Werror -I/usr/local/include -o ./sqlDatabase.o -c ./sqlDatabase.cpp
 ar rc ./libsqlDatabase.a ./sqlDatabase.o
 mv ./libsqlDatabase.a /usr/local/lib/libsqlDatabase.a
 cp ./sqlDatabase.h /usr/include/mysql/sqlDatabase.h
